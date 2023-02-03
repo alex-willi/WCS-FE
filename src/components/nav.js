49f83630bd14f339463a662f2d8ea4f0280110f8
alex-willi <alex.lawson.williams@gmail.com>
2023-02-03 13:30:40 -0600

    update nav bar git add .

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,7 +1,17 @@
 import { Link } from "react-router-dom";
 import { getUserToken, clearUserToken } from "../utils/authToken";
+import { useState, useEffect } from "react";
 export default function Nav() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const token = getUserToken();
+  useEffect(() => {
+    setIsLoggedIn(!!token);
+    console.log(typeof !!token);
+  }, [token]);
+  const handleLogOut = () => {
+    clearUserToken();
+    setIsLoggedIn(false);
+  };
   return (
     <div>
       <nav>
@@ -11,11 +21,10 @@ export default function Nav() {
         <Link to={"/contact"}>Contact Us&nbsp;</Link>
         {token ? (
           ((<Link to={"/Profile"}>Profile</Link>),
-          (<button onClick={() => clearUserToken()}>Log Out</button>))
+          (<button onClick={() => handleLogOut()}>Log Out</button>))
         ) : (
           <Link to={"/Auth"}>Log In&nbsp;</Link>
         )}
-        <Link to={"/Auth"}>Log In&nbsp;</Link>
       </nav>
     </div>
   );
