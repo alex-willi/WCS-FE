18a19318ddf247fcca9a2b3e26b7cfd437a162c0
alex-willi <alex.lawson.williams@gmail.com>
2023-02-03 14:47:39 -0600

    remove strict mode

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ import "semantic-ui-css/semantic.min.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <App />
   </Router>
 );
