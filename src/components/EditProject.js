f25b51a2c4a43fdce7f0dea255de30b71e04b29d
alex-willi <alex.lawson.williams@gmail.com>
2023-02-02 21:15:05 -0600

    add delete handler to project edit page

--- src/components/EditProject.js
+++ src/components/EditProject.js
@@ -33,37 +33,68 @@ const EditProject = () => {
       console.error(error);
     }
   };
+  const handleDelete = async (e) => {
+    e.preventDefault();
+    try {
+      const requestOptions = {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      const response = await fetch(
+        `http://localhost:4000/project/${id}`,
+        requestOptions
+      );
+      const deletedAssociate = await response.json();
+      console.log(deletedAssociate);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
-    <form>
-      <label htmlFor="name">Name</label>
-      <input
-        id="name"
-        type="text"
-        name="name"
-        value={projectForm.name}
-        onChange={handleChange}
-      />
-      <label htmlFor="description">Description</label>
-      <input
-        id="description"
-        type="text"
-        name="description"
-        value={projectForm.description}
-        onChange={handleChange}
-      />
-      <label htmlFor="client">Client</label>
-      <input
-        id="client"
-        type="text"
-        name="client"
-        value={projectForm.client}
-        onChange={handleChange}
-      />
-      <button onSubmit={handleSubmit} type="submit">
-        Save Changes
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          id="name"
+          type="text"
+          name="name"
+          value={projectForm.name}
+          onChange={handleChange}
+        />
+        <label htmlFor="description">Description</label>
+        <input
+          id="description"
+          type="text"
+          name="description"
+          value={projectForm.description}
+          onChange={handleChange}
+        />
+        <label htmlFor="client">Client</label>
+        <input
+          id="client"
+          type="text"
+          name="client"
+          value={projectForm.client}
+          onChange={handleChange}
+        />
+        <label htmlFor="associates">associates</label>
+        <input
+          id="associates"
+          type="text"
+          name="associates"
+          value={projectForm.associates}
+          onChange={handleChange}
+        />
+        <button type="submit">Save Changes</button>
+      </form>
+      <button onClick={handleDelete} type="buttom">
+        DELETE
       </button>
-    </form>
+    </div>
   );
 };
 
