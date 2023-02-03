0d9b4a830661bb8231fa3f3350ee30c0d0b30258
alex-willi <alex.lawson.williams@gmail.com>
2023-02-02 18:49:08 -0600

    make a projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,34 @@
-export default function Projects() {
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const ProjectsPage = () => {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    async function fetchProjects() {
+      const response = await fetch("http://localhost:4000/project");
+      const data = await response.json();
+      setProjects(data);
+    }
+
+    fetchProjects();
+  }, []);
+  console.log(projects);
   return (
     <div>
-      <p>projects</p>
+      <h2>Projects</h2>
+      {projects.map((project) => (
+        <div key={project._id}>
+          <Link to={`/project/${project.id}`}>
+            <h3>{project.name}</h3>
+          </Link>
+          <p>Description: {project.description}</p>
+          <p>Client: {project.client}</p>
+          <p>Associates: {project.associates}</p>
+        </div>
+      ))}
     </div>
   );
-}
+};
+
+export default ProjectsPage;
