4983681b65bd3ce319de5b60b3a035e270d2b504
alex-willi <alex.lawson.williams@gmail.com>
2023-02-05 18:36:32 -0600

    add bootstrap to advert and graphic pages

--- src/pages/Advertisement.js
+++ src/pages/Advertisement.js
@@ -2,44 +2,71 @@ import React from "react";
 
 const Advertisement = () => {
   return (
-    <div className="advertisement-page">
-      <h1 className="advertisement-page__title">Advertisement Services</h1>
-      <p className="advertisement-page__intro">
-        At End-to-End Solutions, we offer a wide range of advertising services
-        to help our clients reach their target audience and increase their
-        online visibility.
-      </p>
-      <div className="advertisement-page__services">
-        <div className="advertisement-page__services-item">
-          <h2 className="advertisement-page__services-item-title">
-            Google and Facebook Ad Campaigns
-          </h2>
-          <p className="advertisement-page__services-item-desc">
-            Our ad campaign services include creating and managing effective
-            Google and Facebook ad campaigns. We work closely with our clients
-            to understand their target audience and create ads that effectively
-            reach and convert them.
+    <div className="container advertisement-page">
+      <div className="row">
+        <div className="col-12 text-center">
+          <h1 className="advertisement-page__title">Advertisement Services</h1>
+          <p className="advertisement-page__intro">
+            At End-to-End Solutions, we offer a wide range of advertising
+            services to help our clients reach their target audience and
+            increase their online visibility.
           </p>
         </div>
-        <div className="advertisement-page__services-item">
-          <h2 className="advertisement-page__services-item-title">
-            Social Media Marketing
-          </h2>
-          <p className="advertisement-page__services-item-desc">
-            Our social media marketing services include developing and
-            implementing effective social media marketing strategies. We help
-            our clients increase their online presence and engagement with their
-            target audience.
-          </p>
+        <div className="col-12 col-md-4 mb-3">
+          <div className="card">
+            <div className="card-body">
+              <h2 className="advertisement-page__services-item-title text-center">
+                Google and Facebook Ad Campaigns
+              </h2>
+              <p className="advertisement-page__services-item-desc text-center">
+                Our ad campaign services include creating and managing effective
+                Google and Facebook ad campaigns. We work closely with our
+                clients to understand their target audience and create ads that
+                effectively reach and convert them.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-12 col-md-4 mb-3">
+          <div className="card">
+            <div className="card-body">
+              <h2 className="advertisement-page__services-item-title text-center">
+                Social Media Marketing
+              </h2>
+              <p className="advertisement-page__services-item-desc text-center">
+                Our social media marketing services include developing and
+                implementing effective social media marketing strategies. We
+                help our clients increase their online presence and engagement
+                with their target audience.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-12 col-md-4 mb-3">
+          <div className="card">
+            <div className="card-body">
+              <h2 className="advertisement-page__services-item-title text-center">
+                Content Marketing
+              </h2>
+              <p className="advertisement-page__services-item-desc text-center">
+                Our content marketing services focus on creating and
+                distributing valuable, relevant, and consistent content to
+                attract and retain a clearly-defined target audience and
+                ultimately drive profitable customer action.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-      <p className="advertisement-page__cta">
-        Thank you for considering our services. Please feel free to{" "}
-        <a className="advertisement-page__cta-link" href="#">
-          contact us
-        </a>{" "}
-        for more information or to request a quote.
-      </p>
+      <div className="col-12 text-center">
+        <p className="advertisement-page__cta">
+          Thank you for considering our services. Please feel free to{" "}
+          <a className="advertisement-page__cta-link" href="#">
+            contact us
+          </a>{" "}
+          for more information or to request a quote.
+        </p>
+      </div>
     </div>
   );
 };
--- src/pages/GraphicDesign.js
+++ src/pages/GraphicDesign.js
@@ -1,55 +1,74 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const GraphicDesign = () => {
   return (
-    <div className="graphic-design-page">
-      <h1 className="graphic-design-page__title">Graphic Design Services</h1>
-      <p className="graphic-design-page__intro">
-        At End-to-End Solutions, we offer a wide range of graphic design
-        services to help our clients effectively communicate their message and
-        stand out in their industry.
-      </p>
-      <div className="graphic-design-page__services">
-        <div className="graphic-design-page__services-item">
-          <h2 className="graphic-design-page__services-item-title">
-            Logo Design
-          </h2>
-          <p className="graphic-design-page__services-item-desc">
-            Our logo design services include creating unique and memorable logos
-            that effectively represent your brand. We work closely with our
-            clients to understand their vision and create a logo that accurately
-            reflects their company's values and mission.
+    <div className="container graphic-design-page">
+      <div className="row">
+        <div className="col-12 text-center">
+          <h1 className="graphic-design-page__title">
+            Graphic Design Services
+          </h1>
+          <p className="graphic-design-page__intro">
+            At End-to-End Solutions, we offer a wide range of graphic design
+            services to help our clients effectively communicate their message
+            and stand out in their industry.
           </p>
         </div>
-        <div className="graphic-design-page__services-item">
-          <h2 className="graphic-design-page__services-item-title">
-            Brochure Design
-          </h2>
-          <p className="graphic-design-page__services-item-desc">
-            Our brochure design services include creating visually appealing
-            brochures that effectively communicate your message. We work with
-            our clients to understand their target audience and create a design
-            that is both informative and engaging.
-          </p>
+        <div className="col-12 col-md-4 mb-3">
+          <div className="card">
+            <div className="card-body">
+              <h2 className="graphic-design-page__services-item-title text-center">
+                Logo Design
+              </h2>
+              <p className="graphic-design-page__services-item-desc text-center">
+                Our logo design services include creating unique and memorable
+                logos that effectively represent your brand. We work closely
+                with our clients to understand their vision and create a logo
+                that accurately reflects their company's values and mission.
+              </p>
+            </div>
+          </div>
         </div>
-        <div className="graphic-design-page__services-item">
-          <h2 className="graphic-design-page__services-item-title">
-            Business Card Design
-          </h2>
-          <p className="graphic-design-page__services-item-desc">
-            Our business card design services include creating professional and
-            unique business cards that accurately reflect your brand. We work
-            closely with our clients to create a design that stands out and
-            effectively communicates your contact information.
-          </p>
+        <div className="col-12 col-md-4 mb-3">
+          <div className="card">
+            <div className="card-body">
+              <h2 className="graphic-design-page__services-item-title text-center">
+                Brochure Design
+              </h2>
+              <p className="graphic-design-page__services-item-desc text-center">
+                Our brochure design services include creating visually appealing
+                brochures that effectively communicate your message. We work
+                with our clients to understand their target audience and create
+                a design that is both informative and engaging.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-12 col-md-4 mb-3">
+          <div className="card">
+            <div className="card-body">
+              <h2 className="graphic-design-page__services-item-title text-center">
+                Business Card Design
+              </h2>
+              <p className="graphic-design-page__services-item-desc text-center">
+                Our business card design services include creating professional
+                and unique business cards that accurately reflect your brand. We
+                work closely with our clients to create a design that stands out
+                and effectively communicates your contact information.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-      <p className="graphic-design-page__cta">
-        Thank you for considering our services. Please feel free to{" "}
-        <Link to="/contact">contact us</Link> for more information or to request
-        a quote.
-      </p>
+      <div className="col-12 text-center">
+        <p className="graphic-design-page__cta">
+          Thank you for considering our services. Please feel free to{" "}
+          <a className="graphic-design-page__cta-link" href="#">
+            contact us
+          </a>{" "}
+          for more information or to request a quote.
+        </p>
+      </div>
     </div>
   );
 };
